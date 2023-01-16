48ac7c85ef6a24ae0780fa530bae7fb40f224d74
1sahinomer1 <1sahinomer1@gmail.com>
2023-01-16 21:07:56 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import { Route, Routes } from "react-router";
+import { BrowserRouter } from "react-router-dom";
+import { Home } from "./pages";
+
+function App() {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnMount: false,
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
+  return (
+    <div>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
+    </div>
+  );
+}
+
+export default App;
